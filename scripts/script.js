9e99889fbaaeb5095b74efc719c3a3c49c7aaf1a
Inna Kovtun <39884162+InnaKovtun@users.noreply.github.com>
2020-07-16 18:26:48 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const  dataBase = JSON.parse(localStorage.getItem('awito')) || [ ];
-
+let counter = dataBase.length;
 //добавляем в переменную кнопку
 const modalAdd = document.querySelector('.modal__add'),
     addAd = document.querySelector('.add__ad'),
@@ -14,7 +14,17 @@ const modalAdd = document.querySelector('.modal__add'),
     modalFileBtn = document.querySelector('.modal__file-btn'),
     modalImageAdd = document.querySelector('.modal__image-add');
 
+const modalImageItem = document.querySelector('.modal__image-item'),
+    modalHeaderItem = document.querySelector('.modal__header-item'),
+    modalStatusItem = document.querySelector('.modal__status-item'),
+    modalDescriptionItem = document.querySelector('.modal__description-item'),
+    modalCostItem = document.querySelector('.modal__cost-item');
+
+const searchInput = document.querySelector('.search__input'),
+    menuContainer = document.querySelector('.menu__container');
+
 const textFileBtn = modalFileBtn.textContent;
+
 const srcModelImage = modalImageAdd.src;
 
 const elementsModalSubmit = [...modalSubmit.elements]
@@ -48,21 +58,33 @@ const closeModal = event => {
     } 
 }
  
-const renderCard = () => {
+const renderCard = (DB = dataBase) => {
     catalog.textContent = '';
-    dataBase.forEach((item, i) =>{
+
+    DB.forEach(item =>{
         catalog.insertAdjacentHTML('beforeend', `
-            <li class="card" data-id = "${i}">
+            <li class="card" data-id-item = "${item.id}">
                 <img class="card__image" src="data:image/jpeg;base64,${item.image}" alt="test">
                 <div class="card__description">
                     <h3 class="card__header">${item.nameItem}</h3>
-                    <div class="card__price">${item.costItem}</div>
+                    <div class="card__price">${item.costItem} грн</div>
                 </div>
             </li>
         `);
     });
 };
 
+searchInput.addEventListener('input', () => {
+    // trim - обрезает пробелы спереди и сзади (   -    ) -> (-)
+    const valueSearch = searchInput.value.trim().toLowerCase();
+
+    if(valueSearch.length > 2){
+        const result = dataBase.filter(item => item.nameItem.toLowerCase().includes(valueSearch) || 
+                        item.descriptionItem.toLowerCase().includes(valueSearch));
+        renderCard(result);
+        
+    }
+});
 
 modalFileInput.addEventListener('change', () => {
     const target = event.target;
@@ -74,7 +96,7 @@ modalFileInput.addEventListener('change', () => {
     infoPhoto.filename = file.name;
     infoPhoto.size = file.size;
 
-    reader.readAsBinaryString(file);
+    reader.readAsBinaryString(file);  
     console.log(target.files[0]);
     
     reader.addEventListener('load', event => {
@@ -99,6 +121,7 @@ modalSubmit.addEventListener('submit', event => {
     const itemObj = {}; //создаем объект 
     for (const elem of elementsModalSubmit){
          itemObj[elem.name] = elem.value;
+         itemObj.id = counter++; //уникальное число для каждого объявления
     } //заполняем его инфой о товаре
     itemObj.image = infoPhoto.base64;
     dataBase.push(itemObj); //добавляем в массив
@@ -118,11 +141,30 @@ addAd.addEventListener('click', ()=> {
 //открываем модальное окно при нажатии на карточку товара
 catalog.addEventListener('click', event => {
     const target = event.target;
+    const card = target.closest('.card');
     //если у таргет родительский класс кард, то открываем модальное окно
-    if(target.closest('.card')){
+    if(card){
+        const item = dataBase.find(obj => obj.id === parseInt(card.dataset.idItem)) ;
+
+        //заполняем модальные окна объявлений с базы
+        modalImageItem.src = `data:image/jpeg;base64,${item.image}`;
+        modalHeaderItem.textContent = item.nameItem;
+        modalStatusItem.textContent = item.status === 'new' ? 'Новый' : 'Б/У';
+        modalDescriptionItem.textContent = item.descriptionItem;
+        modalCostItem.textContent = item.costItem + ' грн';
         modalItem.classList.remove('hide');
         document.addEventListener('keydown', closeModal);
     };
+});
+
+//поиск по разделам меню
+menuContainer.addEventListener('click', event => {
+    const target = event.target;
+
+    if(target.tagName === 'A'){
+        const result = dataBase.filter(item => item.category === target.dataset.category);
+        renderCard(result);
+    }
 });
 
 //закрытие модальных окон
